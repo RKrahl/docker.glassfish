61e114c2b6afa13d6e7afc3eb97a690d40ef9b16
Rolf Krahl <rolf.krahl@helmholtz-berlin.de>
2016-04-20 14:29:00 +0200

    Add an init script as entry point to configure GlassFish before starting.

--- Dockerfile
+++ Dockerfile
@@ -2,30 +2,38 @@ FROM opensuse
 
 ENV GLASSFISH_HOME /opt/glassfish4
 ENV JAVA_HOME /usr/lib64/jvm/java-1.8.0-openjdk
-ENV PATH $GLASSFISH_HOME/bin:$JAVA_HOME/bin:/usr/local/bin:/usr/bin:/bin 
 
 RUN zypper --non-interactive modifyrepo --disable non-oss update-non-oss
 RUN zypper --non-interactive install \
 	curl \
 	java-1_8_0-openjdk-devel \
+	mysql-connector-java \
 	net-tools \
 	pwgen \
 	unzip
 
-RUN /usr/sbin/groupadd -r glassfish && \
-    /usr/sbin/useradd -r -g glassfish -d $GLASSFISH_HOME -s /sbin/nologin \
+RUN groupadd -r glassfish && \
+    useradd -r -g glassfish -d $GLASSFISH_HOME -s /sbin/nologin \
 	-c "GlassFish JavaEE application server" glassfish && \
-    /usr/bin/mkdir -p $GLASSFISH_HOME && \
-    /usr/bin/chown glassfish:glassfish $GLASSFISH_HOME
+    mkdir -p $GLASSFISH_HOME /etc/glassfish /etc/glassfish.d
+COPY docker-entrypoint.sh /etc/glassfish
+RUN chmod 0755 /etc/glassfish/docker-entrypoint.sh && \
+    chown -R glassfish:glassfish $GLASSFISH_HOME /etc/glassfish /etc/glassfish.d
 
 USER glassfish
 WORKDIR $GLASSFISH_HOME
+
+ENV PATH $GLASSFISH_HOME/bin:$JAVA_HOME/bin:/usr/local/bin:/usr/bin:/bin
 
 RUN tmpfile=`mktemp` && \
     curl --silent --show-error --location --output $tmpfile \
 	http://download.java.net/glassfish/4.0/release/glassfish-4.0.zip && \
     unzip -q -d /opt $tmpfile && \
-    rm -rf $tmpfile
+    rm -rf $tmpfile && \
+    asadmin delete-domain domain1
+
+ENTRYPOINT ["/etc/glassfish/docker-entrypoint.sh"]
+
+VOLUME ["/opt/glassfish4/glassfish/domains"]
 
 EXPOSE 4848 8080 8181
-CMD asadmin start-domain --verbose
---
+++ docker-entrypoint.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+domaindir=/opt/glassfish4/glassfish/domains
+domainname=domain1
+
+
+glassfish_init() {
+    adminpw="$(pwgen -s 32 1)"
+    pwfile=$(mktemp)
+    echo "AS_ADMIN_PASSWORD=${adminpw}" > $pwfile
+    echo "ADMIN PASSWORD = ${adminpw}"
+    asadmin --passwordfile $pwfile --user admin \
+	create-domain --savelogin $domainname
+    rm -f $pwfile
+    asadmin start-domain $domainname
+    asadmin enable-secure-admin
+    asadmin stop-domain $domainname
+    if [ ! -z "$GF_DOMAIN_LIBS" ]; then
+	for f in ${GF_DOMAIN_LIBS}; do
+	    test -f $f && ln -s $f $domaindir/$domainname/lib
+	done
+    fi
+    asadmin start-domain $domainname
+    asadmin set server.http-service.access-log.format="common"
+    asadmin set server.http-service.access-logging-enabled=true
+    asadmin set server.thread-pools.thread-pool.http-thread-pool.max-thread-pool-size=128
+    asadmin set configs.config.server-config.cdi-service.enable-implicit-cdi=false
+    asadmin set server.ejb-container.property.disable-nonportable-jndi-names="true"
+    asadmin delete-ssl --type http-listener http-listener-2
+    asadmin delete-network-listener http-listener-2
+    asadmin create-network-listener --listenerport 8181 --protocol http-listener-2 http-listener-2
+    asadmin create-ssl --type http-listener --certname s1as --ssl3enabled=false --ssl3tlsciphers +TLS_RSA_WITH_AES_256_CBC_SHA,+TLS_RSA_WITH_AES_128_CBC_SHA http-listener-2
+    asadmin set configs.config.server-config.network-config.protocols.protocol.http-listener-2.http.request-timeout-seconds=-1
+
+    for f in /etc/glassfish.d/*; do
+	case "$f" in
+	    *.sh)    echo "running $f"; . "$f" ;;
+	    *)       echo "ignoring $f" ;;
+	esac
+    done
+
+    asadmin stop-domain $domainname
+}
+
+if [[ ! -d $domaindir/domain1 ]]; then
+    glassfish_init
+fi
+
+exec asadmin start-domain --watchdog
