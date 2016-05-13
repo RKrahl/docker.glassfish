d0a95d2fcbb9a51aa895daa6594050c885c91702
Rolf Krahl <rolf.krahl@helmholtz-berlin.de>
2016-05-13 12:46:49 +0200

    Add the root certificate to Glassfish's cacerts.jks keystore if needed.

--- start-glassfish.sh
+++ start-glassfish.sh
@@ -28,9 +28,32 @@ glassfish_init() {
 	# self-signed certificate that Glassfish created during
 	# create-domain.
 	tmpfile=`mktemp`
+	# Remove the self-signed certificate from Glassfish's cacerts.jks.
+	keytool -delete -alias s1as \
+		-keystore $domaindir/config/cacerts.jks -storetype jks \
+		-storepass changeit \
+		-noprompt
+	# Check whether the root ca is already in cacerts.jks and add
+	# it if needed.
+	rootfp=`openssl x509 -in $certsdir/rootcert.pem \
+                    -noout -sha1 -fingerprint | cut -d '=' -f 2 -s`
+	if ! (keytool -list -keystore $domaindir/config/cacerts.jks \
+	          -storetype jks -storepass changeit \
+	          | grep -q $rootfp); then
+	    echo "Import root cert to cacerts.jks"
+	    # Choose a random alias for the new ca entry that is very
+	    # unlikely to collide with an already existing one.
+	    alias=`pwgen 32 1`
+	    openssl x509 -in $certsdir/rootcert.pem -outform der -out $tmpfile
+	    keytool -import -file $tmpfile -alias $alias \
+		-keystore $domaindir/config/cacerts.jks -storetype jks \
+		-storepass changeit \
+		-noprompt
+	fi
+	echo "Import cert.pem to keystore.jks"
 	openssl pkcs12 -export -chain \
 	    -in $certsdir/cert.pem -inkey $certsdir/key.pem \
-	    -CAfile $certsdir/cacert.pem \
+	    -CAfile $certsdir/certchain.pem \
 	    -out $tmpfile -name s1as -passout pass:changeit
 	keytool -importkeystore \
 	    -srckeystore $tmpfile -srcstoretype pkcs12 \
